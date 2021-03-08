415cd4811b95a9e38cbc5378919b9e466bd5b077
Andy Trofimov <webgrandmo@gmail.com>
2021-03-08 17:08:33 +0200

    create backend and .gitignore

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,9 +4,29 @@ const mognodb = require('mongodb');
 
 const router = express.Router();
 
+// Get posts
 router.get('/', async (req, res) => {
 	const posts = await loadPostCollection();
 	res.send(await posts.find({}).toArray());
+});
+
+// Add posts
+
+router.post('/', async (req, res) => {
+	const posts = await loadPostCollection();
+	await posts.insertOne({
+		text: req.body.text,
+		createdAt: new Date(),
+	});
+	res.status(201).send();
+});
+
+// Delete post
+
+router.delete('/:id', async (req, res) => {
+	const posts = await loadPostCollection();
+	await posts.deleteOne({ _id: new mognodb.ObjectID(req.params.id) });
+	res.status(200).send();
 });
 
 const uri =
